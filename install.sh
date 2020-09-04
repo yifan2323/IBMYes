d9b1428f1d784fdd7ba3ef3a34780145936bc85e
zcmimi <36293036+zcmimi@users.noreply.github.com>
2020-09-04 17:48:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ clone_repo(){
     echo "进行初始化。。。"
     git clone https://github.com/CCChieh/IBMYes
     cd IBMYes
-    git submodule update --init --recursive
+    git clone https://github.com/zcmimi/v2ray-cloudfoundry
     cd v2ray-cloudfoundry/v2ray
     # Upgrade V2Ray to the latest version
     rm v2ray v2ctl
